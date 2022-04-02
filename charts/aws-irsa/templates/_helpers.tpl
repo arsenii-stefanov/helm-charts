f65714e1c32da67d7e0b21effbbf56297b3c2c41
Arsenii Stefanov <arsenii.stefanov@local>
2022-04-02 20:08:50 +0300

    Release aws-irsa ver. 0.0.1

--- charts/aws-irsa/templates/_helpers.tpl
+++
@@ -1,4 +0,0 @@
-{{- define "irsa.cluster_tags" -}}
-- key: cluster_name
-  value: {{ .Values.clusterName }}
-{{- end -}}
--- scripts/aux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-### Colours
-LGREEN=$'\033[1;32m' # Light Green
-LCYAN=$'\033[1;36m' # Light Cyan
-LYLW=$'\033[1;33m' # Light Yellow
-LRED=$'\033[1;31m' # Light Red
-NOCOLOR=$'\033[0m' # No Color
